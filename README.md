aca09027b07c91fef71546bdf41300dd884f9570
Guillaume Blin <guillaume.blin@labri.fr>
2016-03-07 09:43:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Man pages Local Plugin for Moodle 2.2+
+==================================================
+
+This is a package of Linux man pages for Moodle. Man Pages Copyright Respective Owners. 
+
+Description
+---------------
+
+This plugin allows to retrieve the man pages of the local server.
+
+Installation
+-----------------
+
+To install the plugin using git, execute the following commands in the root of your Moodle install:
+
+    git clone https://github.com/GuillaumeBlin/moodle-local_manpages.git
+    echo '/local/manpages' >> .git/info/exclude
+    
+Or, extract the following zip in your_moodle_root/local/:
+
+    https://github.com/GuillaumeBlin/moodle-local_manpages/zipball/master
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Manpages local plugin. 
+ * Adds support for man pages.
+ *
+ * @package   local_manpages
+ * @copyright 2016 Universite de Bordeaux
+ * @author    Guillaume Blin <guillaume.blin@labri.fr>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+?>
+<pre>
+<?php
+	$cmd="man";
+	if(isset($_GET["cmd"])){
+		$cmd=$_GET["cmd"];
+	}
+	passthru("man ".$cmd." 2>&1");
+?>
+</pre>
